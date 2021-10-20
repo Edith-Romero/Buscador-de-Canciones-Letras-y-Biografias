b4d10607e3328729bc65b783ac58d1edba48d678
Edith Romero <edithjohanaromero@gmail.com>
2021-10-19 21:46:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment,useState,useEffect} from 'react';
+import Formulario from './Components.jsx/Formulario';
+import Cancion from './Components.jsx/Cancion';
+import Info from './Components.jsx/Info';
+import axios from 'axios';
 
 function App() {
+
+  // Definir el State
+  const [busquedaletra, setBusquedaLetra] = useState({});
+  const [letra,setLetra] = useState('');
+  const [info,setInfo] = useState({});
+  
+  useEffect(()=>{
+    // Prevenir que el objeto este vacio
+    if(Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async() =>{
+
+      const {artista,cancion} = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+      const url2= `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`
+      // Con una promise hago el llamado a las dos api de una sola vez
+      const [letra,informacion] = await Promise.all([
+        axios(url),
+        axios(url2)
+      ]);
+
+      setLetra(letra.data.lyrics);
+      setInfo(informacion.data.artists[0]);
+    }
+    consultarApiLetra();
+
+  },[busquedaletra,info])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario
+        setBusquedaLetra={setBusquedaLetra}
+      />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col md-6">
+            <Info
+              info={info}
+            />
+          </div>
+          <div className="col md-6">
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
